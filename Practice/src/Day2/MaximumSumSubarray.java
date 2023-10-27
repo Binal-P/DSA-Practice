1985740af68a0bc3480cf05675db28c599eae30d
Binal Patel <binalpatel@Binals-MBP.fios-router.home>
2023-10-26 23:24:02 -0400

    Day 2 Problem 1

---
+++ Practice/src/Day2/MaximumSumSubarray.java
@@ -0,0 +1,21 @@
+package Day2;
+
+public class MaximumSumSubarray {
+    //Given an integer array nums, find the
+    //subarray
+    // with the largest sum, and return its sum.
+    //TC: O(n)
+    //SC: O(1)
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum =0;
+        for(int i=0;i<nums.length;i++){
+            sum = sum+=nums[i];
+            max = Math.max(sum,max);
+            if(sum<0){
+                sum =0;
+            }
+        }
+        return max;
+    }
+}
