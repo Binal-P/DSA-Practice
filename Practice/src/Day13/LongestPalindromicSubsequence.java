73e6bef4d22c8ff5b485420c139e32db9d203cf6
Binal Patel <binalpatel@Binals-MBP.fios-router.home>
2023-11-08 23:36:59 -0500

    More Problems

---
+++ Practice/src/Day13/LongestPalindromicSubsequence.java
@@ -0,0 +1,49 @@
+package Day13;
+
+import java.util.Arrays;
+
+public class LongestPalindromicSubsequence {
+    //Given a string s, find the longest palindromic subsequence's length in s.
+    //
+    //A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+    //
+    //
+    //
+    //Example 1:
+    //
+    //Input: s = "bbbab"
+    //Output: 4
+    //Explanation: One possible longest palindromic subsequence is "bbbb".
+    //Example 2:
+    //
+    //Input: s = "cbbd"
+    //Output: 2
+    //Explanation: One possible longest palindromic subsequence is "bb".
+    int[][] dp = new int[1000][1000];
+    public int longestPalindromeSubseq(String s) {
+        for(int[] ar:dp){
+            Arrays.fill(ar,-1);
+        }
+
+        return longestPalindrome(s,0,s.length()-1);
+    }
+    public int longestPalindrome(String s, int i, int j){
+        if(i==j){
+            return 1;
+        }
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int max = 0;
+        if(s.charAt(i)==s.charAt(j)){
+            max= 2+ longestPalindrome(s,i+1,j-1);
+        }else{
+            max= Math.max(longestPalindrome(s,i,j-1),longestPalindrome(s,i+1,j));
+        }
+        dp[i][j] = max;
+        return max;
+    }
+}
---
+++ Practice/src/Day13/SubsetSumGFG.java
@@ -0,0 +1,37 @@
+package Day13;
+
+import java.util.Arrays;
+
+public class SubsetSumGFG {
+    //Given an array of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum.
+    //https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+    static Boolean isSubsetSum(int n, int arr[], int sum){
+        int[][] dp = new int[n+1][sum+1];
+        for(int[] ar:dp){
+            Arrays.fill(ar,-1);
+        }
+        boolean ans = false;
+        if(f(n,sum,arr,dp) ==1){
+            ans =  true;
+        }
+        return ans;
+    }
+    static int f(int n, int sum, int[] arr,int[][] dp ){
+        if(sum == 0){
+            return 1;
+        }
+        if(n<=0){
+            return 0;
+        }
+        if(dp[n-1][sum]!=-1){
+            return dp[n-1][sum];
+        }
+        if(arr[n-1] >sum){
+            return dp[n-1][sum] = f(n-1,sum,arr,dp);
+        } else if(f(n-1,sum,arr,dp) != 0 || f(n - 1, sum - arr[n - 1],arr,dp)!= 0){
+            return dp[n-1][sum]=1;
+        } else{
+            return dp[n-1][sum]=0;
+        }
+    }
+}
